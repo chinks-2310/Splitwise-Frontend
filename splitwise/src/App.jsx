a33c8b63052b53f837242d8125fcaaaa2c255724
chintan.thakkar <201701141@daiict.ac.in>
2021-05-14 12:31:49 +0530

    added header and footer to app.jsx

--- splitwise/src/App.jsx
+++ splitwise/src/App.jsx
@@ -1,10 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Router>
+      <Header/>
+      <main className="py-3">
+        <Container></Container>
+      </main>
+      <Footer/>
+    </Router>
   );
 }
 
